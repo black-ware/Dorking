7b4a5ef2480cc56a3ebbcc6e18195e1599c09e9f
BlackWare Coder Team <46942910+black-ware@users.noreply.github.com>
2019-01-23 05:19:54 +0700

    Add files via upload

---
+++ Dork.sh
@@ -0,0 +1,98 @@
+#Copyright 2k19 - BlackWare
+clear
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+versi='V 1.4'
+echo $g""
+figlet BlackWare
+echo
+echo $w"_"$r"======================================"$w"_"
+echo
+echo $r"["$w"+"$r"]"$w" Author Tools : ELEKTRON_425"
+echo $r"["$w"+"$r"]"$w" Team         : BlackWare"
+echo $r"["$w"+"$r"]"$w" Thanks To    : D4RK_LIFE - Mr.Ki9N - Joker.Id -"" M®.D¥$T®0¥€®"
+echo $r"["$w"+"$r"]"$w" Tools        : Dorker Tools $versi"
+echo $r"["$w"+"$r"]"$w" Note         : Gunakan Dengan Bijak !!!"
+echo
+echo $w"_"$r"======================================"$w"_"
+echo
+echo $r"["$w"+"$r"]"$w" Click Y And Enter To Skip !! =>"
+read -p "" skip
+if [ $skip = "y" ] || [ $skip = "Y" ]
+then
+sleep 2
+clear
+echo $g
+figlet BlackWare
+echo
+echo $w"_"$r"======================================"$w"_"
+echo
+echo $r"["$w"+"$r"]"$w" Author Tools : ELEKTRON_425"
+echo $r"["$w"+"$r"]"$w" Team         : BlackWare"
+echo $r"["$w"+"$r"]"$w" Thanks To    : D4RK_LIFE - Mr.Ki9N - Joker.Id - M®.D¥$T®0¥€®"
+echo $r"["$w"+"$r"]"$w" Tools        : Dorker Tools $versi"
+echo $r"["$w"+"$r"]"$w" Note         : Gunakan Dengan Bijak !!!"
+echo
+echo $w"_"$r"======================================"$w"_"
+echo
+echo $r"_"$w"======================================"$r"_"
+echo $r"_"$r"         ["$w"+"$r"]"$w" Dorker Tools"$r" ["$w"+"$r"]"$r"          _"
+echo
+echo
+echo $r"1 )"$w" SQL injection  ||"$r"  2 )"$w" Com_Fabrik  ||"$r"  3 )"$w" Fck Editor"
+echo $r"4 )"$w" Webdav         ||"$r"  5 )"$w" Bypass      ||"$r"  6 )"$w" Kindeditor"
+echo
+echo $r"[+]"$w"==================================="$r"[+]"
+echo
+echo $r"[?]"$w" Pilih Yang Mana Stah ??"
+echo
+read -p "root@BlackWare : " nomor
+fi
+if [ $nomor = "01" ] || [ $nomor = "1" ]
+then
+echo $r" [*] Wait !!!!!!"
+sleep 1
+clear
+xdg-open "https://www.google.com/search?q=inurl:/.php?id="
+fi
+if [ $nomor = "04" ] || [ $nomor = "4" ]
+then
+echo $r" [*] Wait !!!!!!"
+sleep 1
+clear
+xdg-open 'https://www.google.com/search?q=intext:"(IIS)" ext:asp site:co.za'
+fi
+if [ $nomor = "2" ] || [ $nomor = "02" ]
+then
+echo $r "[*] Wait !!!!!"
+sleep 1
+clear
+xdg-open 'https://www.google.com/search?q=inurl:index.php/component/fabrik/ site:.dk'
+fi
+if [ $nomor = "5" ] || [ $nomor = "05" ]
+then
+echo $r "[*] Wait !!!!!"
+sleep 1
+clear
+xdg-open 'https://www.google.com/search?q=intext:admin login'
+fi
+###
+if [ $nomor = "6" ] || [ $nomor = "06" ]
+then
+echo $r "[*] Wait !!!!!"
+sleep 1
+clear
+xdg-open 'https://www.google.com/search?q=inurl:examples/uploadbutton.html'
+fi
+if [ $nomor = "3" ] || [ $nomor = "03" ]
+then
+echo $r "[*] Wait !!!!!"
+sleep 1
+clear
+xdg-open 'https://www.google.com/search?q=inurl:filemanager/uploadtest.html'
+fi
